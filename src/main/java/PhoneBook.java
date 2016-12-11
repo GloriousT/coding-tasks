9600ae37fdc08c379dff5a969ee4870630f8e73c
glorioust <tolyaniy@gmail.com>
2016-12-11 20:08:38 +0100

    Add additional things and tests

---
+++ src/main/java/PhoneBook.java
@@ -0,0 +1,25 @@
+import com.google.common.collect.Sets;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class PhoneBook {
+
+    private Map<String, Set<String>> phoneBook = new HashMap<>();
+
+    public Set<String> get(String person) {
+        Set<String> phones = phoneBook.get(person.toLowerCase());
+        if (null == phones) {
+            return Sets.newHashSet();
+        } else {
+            return Sets.newHashSet(phones);
+        }
+    }
+
+    public void add(String person, String phone) {
+        Set<String> phones = get(person);
+        phones.add(phone);
+        phoneBook.put(person.toLowerCase(), phones);
+    }
+}
---
+++ src/test/java/PhoneBookTest.java
@@ -0,0 +1,60 @@
+import org.junit.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PhoneBookTest {
+
+    private PhoneBook phoneBook = new PhoneBook();
+
+    @Test
+    public void shouldReturnEmptyListForNotKnownPerson() {
+        assertThat(phoneBook.get("non existing")).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnOnePhone() {
+        phoneBook.add("Name", "aPhone");
+
+        Set<String> person = phoneBook.get("Name");
+
+        assertThat(person).containsExactly("aPhone");
+    }
+
+    @Test
+    public void shouldReturnMoreThanOnePhone() {
+        phoneBook.add("Name", "phone1");
+        phoneBook.add("Name", "phone2");
+
+        Set<String> personPhones = phoneBook.get("Name");
+
+        assertThat(personPhones).containsExactlyInAnyOrder("phone1", "phone2");
+    }
+
+    @Test
+    public void shouldReturnPhonesForAppropriatePersons() {
+        phoneBook.add("Name", "phone1");
+        phoneBook.add("Other Name", "phone2");
+
+        assertThat(phoneBook.get("Name")).containsExactly("phone1");
+        assertThat(phoneBook.get("Other Name")).containsExactly("phone2");
+    }
+
+    @Test
+    public void shouldNotReturnDuplicatePhones() {
+        phoneBook.add("Name", "phone1");
+        phoneBook.add("Name", "phone1");
+
+        assertThat(phoneBook.get("Name")).containsOnlyOnce("phone1");
+    }
+
+    @Test
+    public void shouldReturnPhonesForCaseInsensitiveNames() {
+        phoneBook.add("Name", "phone");
+
+        assertThat(phoneBook.get("name")).containsExactly("phone");
+        assertThat(phoneBook.get("NAME")).containsExactly("phone");
+        assertThat(phoneBook.get("nAmE")).containsExactly("phone");
+    }
+}
